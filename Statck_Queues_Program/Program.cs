a580c900164a785b29fbb6071a92abac7c0eb6a0
Sandeep singh <sandy180388@gmail.com>
2022-08-30 13:26:50 +0530

    [Sandeep] peak and pop file in UC_2

--- Statck_Queues_Program/Program.cs
+++ Statck_Queues_Program/Program.cs
@@ -8,12 +8,12 @@ namespace Statck_Queues_Program
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Welcome to Stacks and Queues!");
-            //creation of a list
+            //creating a list
             Linked_List list = new Linked_List();
-            //pushing elements to list
             list.Push(70);
             list.Push(30);
             list.Push(56);
+            list.IsEmpty();
             list.Display();
         }
     }
